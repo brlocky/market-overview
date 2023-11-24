094e23fd9dd2eae273ddac027895a744dfff98d3
Joao Cunha <brlocky@gmail.com>
2023-11-24 19:12:10 +0000

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MiniChart } from "react-ts-tradingview-widgets";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex w-full  justify-center gap-x-2 p-2">
+        
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="USI:ADD" dateRange='1D' />
+        </div>
+
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="USI:TICK" dateRange='1D' />
+        </div>
+
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="USI:UVOL" dateRange='1D' />
+        </div>
+
+      </div>
+
+      <div className="flex w-full  justify-center gap-x-2 p-2">
+        
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="NAS100USD" dateRange='1D'   />
+        </div>
+
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="SPX" dateRange='1D'   />
+        </div>
+
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="QQQ" dateRange='1D'   />
+        </div>
+
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="IWM" dateRange='1D'   />
+        </div>
+      </div>
+
+      <div className="flex w-full  justify-center gap-x-2 p-2">
+        
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="AAPL" dateRange='1D'   />
+        </div>
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="TSLA" dateRange='1D'   />
+        </div>
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="NVDA" dateRange='1D'   />
+        </div>
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="MSFT" dateRange='1D'   />
+        </div>
+  
+
+      </div>
+      <div className="flex w-full  justify-center gap-x-2 p-2">
+        
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="NFLX" dateRange='1D'   />
+        </div>
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="AMZN" dateRange='1D'   />
+        </div>
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="NASDAQ:GOOGL" dateRange='1D'   />
+        </div>
+        <div className="flex-col">
+          <MiniChart colorTheme="dark" width="100%" symbol="META" dateRange='1D'   />
+        </div>
+
+      </div>
+
+
+    </>
   );
 }
 
